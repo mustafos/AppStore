b02d08199eba7ed1b64105b91d51601433d383c1
Mustafa Bekirov <mustafos1999@gmail.com>
2024-03-05 19:47:51 +0100

    fix: secondary bugs like color button

---
+++ Crafty-craft-10-NoSub/Crafty Craft 5/Tab Bar/Create Tab/Addon Creator/AddonCreatorCollection/AddonCreatorMainVC.swift
@@ -0,0 +1,9 @@
+//
+//  AddonCreatorMainVC.swift
+//  Crafty Craft 10
+//
+//  Created by Mustafa Bekirov on 05.03.2024.
+//  Copyright © 2024 Noname Digital. All rights reserved.
+//
+
+import Foundation
--- Crafty-craft-10-Sub/Crafty Craft 5/Tab Bar/Create Tab/SkinsCreator/ListOfCreatedSkinsController/PixelCreator/DrawLogic/MultiCommand.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-
-protocol MultiCommand {
-    func appendAndExecuteSingle(_ command: Command)
-}
