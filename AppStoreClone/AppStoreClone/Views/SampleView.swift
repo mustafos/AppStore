7b2eb9478875a12fb31fb0cf977a8dd07f9f8d82
mustafos <mustafos1999@icloud.com>
2024-06-30 17:37:00 +0200

    feat: observable macro basics

---
+++ AppStoreClone/AppStoreClone/Views/SampleView.swift
@@ -0,0 +1,50 @@
+//
+//  SampleView.swift
+//  AppStoreClone
+//
+//  Created by Mustafa Bekirov on 30.06.2024.
+//
+
+import SwiftUI
+import Observation
+
+//@MainActor
+//class SampleViewModel: ObservableObject {
+//    @Published var count = 0
+//    
+//    func increaseOnBackgroundThread() {
+//        Task {
+//            count += 5
+//        }
+//    }
+//}
+
+@Observable
+class ObservableSampleViewModel {
+    var count = 0
+    
+    func increaseOnBackgroundThread() {
+        Task {
+            count += 5
+        }
+    }
+}
+
+struct SampleView: View {
+    
+//    @StateObject var vm = SampleViewModel()
+    @State var vm = ObservableSampleViewModel()
+    
+    var body: some View {
+        Button {
+            vm.increaseOnBackgroundThread()
+        } label: {
+            Text("Increase by 1: \(vm.count)")
+                .font(.largeTitle)
+        }
+    }
+}
+
+#Preview {
+    SampleView()
+}
